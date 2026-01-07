5ae567f201b99fee4eab747b9bd52431423380bb
forexacademy-id <ngabkowol@gmail.com>
2026-01-07 08:17:57 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 import { auth, db } from "./firebase.js";
-import { onAuthStateChanged, signOut }
-from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-import { doc, getDoc }
-from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { onAuthStateChanged, signOut } from
+  "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { doc, getDoc } from
+  "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
