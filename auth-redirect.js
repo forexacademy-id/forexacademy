d8cca2960932ef6aab4a0779bb3adceeb19c393b
forexacademy-id <ngabkowol@gmail.com>
2026-01-07 05:26:43 -0800

    Delete auth-redirect.js

--- auth-redirect.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } from
-  "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    window.location.href = "dashboard/index.html";
-  }
-});
